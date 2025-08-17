5412309012467e2ec527af13d896cf02a0c337f2
Attol1 <archdigest7@gmail.com>
2025-08-17 08:28:51 +0800

    Use yarn and fix double listening error

--- myApp.js
+++ myApp.js
@@ -2,11 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => {
-  console.log('Useful Programmer Info Security App Started on port ${PORT}`);
-});
 
 
 
@@ -55,7 +50,8 @@ app.listen(PORT, () => {
 
 
 
-module.exports = app;
+
+
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
@@ -65,5 +61,7 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Useful Programmer Info Security App Started on port ${port}`);
 });
+
+module.exports = app;
